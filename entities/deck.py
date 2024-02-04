49e80864781670ac33ccd4dd659f981b6ca299b9
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-02-04 16:43:15 +0300

    Реализовал класс "Колода". При инициализации экземпляра класса, создается список и наполняться 36-ю экзземплярами класса "Карта"

---
+++ entities/deck.py
@@ -0,0 +1,25 @@
+from random import shuffle
+from entities import Card
+
+class Deck:
+
+    def __init__(self):
+        self.cards = []
+        for value in range(9):
+            for suit in range(4):
+                current_card = Card(value=value, suit=suit)
+                self.cards.append(current_card)
+        self.shuffle()
+
+    def get_card(self) -> Card | None:
+        if len(self.cards) == 0:
+            return None
+        return self.cards.pop()
+
+    def shuffle(self) -> None:
+        shuffle(self.cards)
+
+    def __repr__(self):
+        deck = ", ".join([str(card) for card in self.cards])
+        return f"{deck}"
+
